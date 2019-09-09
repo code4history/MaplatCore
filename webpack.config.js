3af3ec312cd6517dc4f0ff4a1ead111743bc52a1
Kohei Otsuka <kochizufan@gmail.com>
2019-09-09 12:59:55 +0900

    babel for node_modules

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
         rules: [
             {
                 test: /\.js$/,
-                exclude: /node_modules/,
+                exclude: /node_modules(?!\/maplat_tin)/,
                 use: {
                     loader: 'babel-loader',
                     query: {
