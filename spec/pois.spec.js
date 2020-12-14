a6f0f322fca68454783ca3dbf05e1569469e2c68
kobakakzu0429 <kobakazu10@gmail.com>
2020-12-14 21:16:24 +0900

    rewrite test for jest, remove testHelper

--- spec/pois.spec.js
+++ spec/pois.spec.js
@@ -1,5 +1,4 @@
 import { normalizeLayers } from '../src/normalize_pois';
-import testHelper from './TestHelper';
 
 describe('normalizePois test', () => {
   for (let i = 1; i <= 3; i++) {
@@ -8,10 +7,16 @@ describe('normalizePois test', () => {
     try {
       testex = require(`./expect/poitest${i}.json`);
     } catch (e) { console.error(e); }
-    it(testin.title, testHelper.helperAsync(async () => {
-      const testout = await normalizeLayers(testin.data, testin.options);
-      //if (i == 1) fs.writeFileSync(`./spec/expect/poitest${i}.json`, JSON.stringify(testout, null, 2));
-      expect(JSON.parse(JSON.stringify(testout))).toEqual(testex);
-    }));
+
+    it(testin.title, async (done) => {
+      try {
+        const testout = await normalizeLayers(testin.data, testin.options)
+        //if (i == 1) fs.writeFileSync(`./spec/expect/poitest${i}.json`, JSON.stringify(testout, null, 2));
+        expect(JSON.parse(JSON.stringify(testout))).toEqual(testex);
+        done()
+      } catch (error) {
+        done(error)
+      }
+    });
   }
 });
--- spec/template.spec.js
+++ spec/template.spec.js
@@ -9,11 +9,13 @@ describe('template_works test', () => {
       testexIcn = require(`./expect/temicntest${i}.json`);
       testexPoi = require(`./expect/tempoitest${i}.json`);
     } catch (e) { console.error(e); }
+
     it(`${testin.title} (Icon)`, () => {
       const testoutIcn = createIconSet(Object.assign({}, testin.data), testin.ancestors[0], testin.ancestors[1], testin.ancestors[2]);
       //if (i == 3) fs.writeFileSync(`./spec/expect/temicntest${i}.json`, JSON.stringify(testoutIcn, null, 2));
       expect(JSON.parse(JSON.stringify(testoutIcn))).toEqual(testexIcn);
     });
+
     it(`${testin.title} (POI)`, () => {
       const testoutPoi = createHtmlFromTemplate(Object.assign({}, testin.data), testin.ancestors[0], testin.ancestors[1], testin.ancestors[2]);
       //if (i == 3) fs.writeFileSync(`./spec/expect/tempoitest${i}.json`, JSON.stringify(testoutPoi, null, 2));
