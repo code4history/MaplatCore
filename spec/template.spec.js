14184f2047999979f90a780aad802d8de8927185
kobakakzu0429 <kobakazu10@gmail.com>
2020-12-14 17:18:12 +0900

    migrate jasmine test to jest test

--- spec/template.spec.js
+++ spec/template.spec.js
@@ -1,24 +1,23 @@
-const fs = require('fs');
-const {createIconSet, createHtmlFromTemplate} = require('../src/template_works');
+import { createIconSet, createHtmlFromTemplate } from '../src/template_works';
 
-describe('template_works test', function() {
-    for (let i=1; i<=3; i++) {
-        const testin = require(`./input/temtest${i}.json`);
-        let testexIcn;
-        let testexPoi;
-        try {
-            testexIcn = require(`./expect/temicntest${i}.json`);
-            testexPoi = require(`./expect/tempoitest${i}.json`);
-        } catch(e) {}
-        it(`${testin.title} (Icon)`, () => {
-            const testoutIcn = createIconSet(Object.assign({}, testin.data), testin.ancestors[0], testin.ancestors[1], testin.ancestors[2]);
-            //if (i == 3) fs.writeFileSync(`./spec/expect/temicntest${i}.json`, JSON.stringify(testoutIcn, null, 2));
-            expect(JSON.parse(JSON.stringify(testoutIcn))).toEqual(testexIcn);
-        });
-        it(`${testin.title} (POI)`, () => {
-            const testoutPoi = createHtmlFromTemplate(Object.assign({}, testin.data), testin.ancestors[0], testin.ancestors[1], testin.ancestors[2]);
-            //if (i == 3) fs.writeFileSync(`./spec/expect/tempoitest${i}.json`, JSON.stringify(testoutPoi, null, 2));
-            expect(JSON.parse(JSON.stringify(testoutPoi))).toEqual(testexPoi);
-        });
-    }
+describe('template_works test', () => {
+  for (let i = 1; i <= 3; i++) {
+    const testin = require(`./input/temtest${i}.json`);
+    let testexIcn;
+    let testexPoi;
+    try {
+      testexIcn = require(`./expect/temicntest${i}.json`);
+      testexPoi = require(`./expect/tempoitest${i}.json`);
+    } catch (e) { console.error(e); }
+    it(`${testin.title} (Icon)`, () => {
+      const testoutIcn = createIconSet(Object.assign({}, testin.data), testin.ancestors[0], testin.ancestors[1], testin.ancestors[2]);
+      //if (i == 3) fs.writeFileSync(`./spec/expect/temicntest${i}.json`, JSON.stringify(testoutIcn, null, 2));
+      expect(JSON.parse(JSON.stringify(testoutIcn))).toEqual(testexIcn);
+    });
+    it(`${testin.title} (POI)`, () => {
+      const testoutPoi = createHtmlFromTemplate(Object.assign({}, testin.data), testin.ancestors[0], testin.ancestors[1], testin.ancestors[2]);
+      //if (i == 3) fs.writeFileSync(`./spec/expect/tempoitest${i}.json`, JSON.stringify(testoutPoi, null, 2));
+      expect(JSON.parse(JSON.stringify(testoutPoi))).toEqual(testexPoi);
+    });
+  }
 });
