1d513ee929af504c85c741f4f211474320bebddf
kobakakzu0429 <kobakazu10@gmail.com>
2020-12-17 14:08:55 +0900

    remove tasks(server, stopserver) in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,53 +1,50 @@
-var gulp = require('gulp'),
-  fs = require('fs-extra'),
-  spawn = require('child_process').spawn,
-  zip = require('gulp-zip'),
-  psaux = require('psaux'),
-  terminate = require('terminate');
+/* eslint-disable no-empty */
+const gulp = require("gulp");
+const fs = require("fs-extra");
+const zip = require("gulp-zip");
 
-gulp.task('zip', function () {
+gulp.task("zip", async () => {
   try {
-    fs.removeSync('./distribution.zip');
-  } catch (e) {
-  }
+    fs.removeSync("./distribution.zip");
+  } catch (e) { }
   try {
-    fs.removeSync('./distribution');
-  } catch (e) {
-  }
+    fs.removeSync("./distribution");
+  } catch (e) { }
   try {
-    fs.removeSync('./example.zip');
-  } catch (e) {
-  }
+    fs.removeSync("./example.zip");
+  } catch (e) { }
   try {
-    fs.removeSync('./example');
-  } catch (e) {
-  }
+    fs.removeSync("./example");
+  } catch (e) { }
 
-  fs.ensureDirSync('./distribution');
-  fs.copySync('./dist', './distribution/dist');
-  fs.copySync('./parts', './distribution/parts');
+  fs.ensureDirSync("./distribution");
+  fs.copySync("./dist", "./distribution/dist");
+  fs.copySync("./parts", "./distribution/parts");
 
-  return new Promise(function (resolve, reject) {
-    gulp.src(['./distribution/**/*'])
-      .pipe(zip('distribution.zip'))
-      .on('error', reject)
-      .pipe(gulp.dest('./'))
-      .on('end', resolve);
-  }).then(function () {
-    fs.moveSync('./distribution', './example');
-    fs.copySync('./index.html', './example/index.html');
-    fs.copySync('./apps', './example/apps');
-    fs.copySync('./maps', './example/maps');
-    fs.copySync('./pois', './example/pois');
-    fs.copySync('./tiles', './example/tiles');
-    return new Promise(function (resolve, reject) {
-      gulp.src(['./example/**/*'])
-        .pipe(zip('example.zip'))
-        .on('error', reject)
-        .pipe(gulp.dest('./'))
-        .on('end', resolve);
-    });
-  }).then(function () {
-    fs.removeSync('./example');
+  await new Promise((resolve, reject) => {
+    gulp
+      .src(["./distribution/**/*"])
+      .pipe(zip("distribution.zip"))
+      .on("error", reject)
+      .pipe(gulp.dest("./"))
+      .on("end", resolve);
   });
+
+  fs.moveSync("./distribution", "./example");
+  fs.copySync("./index.html", "./example/index.html");
+  fs.copySync("./apps", "./example/apps");
+  fs.copySync("./maps", "./example/maps");
+  fs.copySync("./pois", "./example/pois");
+  fs.copySync("./tiles", "./example/tiles");
+
+  await new Promise((resolve, reject) => {
+    gulp
+      .src(["./example/**/*"])
+      .pipe(zip("example.zip"))
+      .on("error", reject)
+      .pipe(gulp.dest("./"))
+      .on("end", resolve);
+  });
+
+  fs.removeSync("./example");
 });
