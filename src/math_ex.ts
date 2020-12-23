60d8cbb3130cc04d4cb47a0e2657548d8d43f3ab
kobakakzu0429 <kobakazu10@gmail.com>
2020-12-24 03:12:37 +0900

    ts-migrate: math_ex

--- src/math_ex.ts
+++ src/math_ex.ts
@@ -1,19 +1,22 @@
-export function randomFromCenter(center, pow) {
+export function randomFromCenter(center: number, pow: number): number {
   return center + (Math.random() - 0.5) * pow;
 }
 
-export function recursiveRound(val, decimal) {
+export function recursiveRound(val: any, decimal: number): any {
   if (val instanceof Array)
     return val.map(item => recursiveRound(item, decimal));
   const decVal = Math.pow(10, decimal);
   return Math.round(val * decVal) / decVal;
 }
 
-function radians(deg) {
+function radians(deg: number): number {
   return (deg * Math.PI) / 180;
 }
 
-export function getDistance(lnglat1, lnglat2) {
+export function getDistance(
+  lnglat1: [number, number],
+  lnglat2: [number, number]
+): number {
   return (
     6378.14 *
     Math.acos(
