610583cbe23861cadbde0d4c1bea6889e86d2f83
kobakakzu0429 <kobakazu10@gmail.com>
2020-12-26 13:13:22 +0900

    add file-loader, url-loader

--- webpack_config/webpack.config.standard.js
+++ webpack_config/webpack.config.standard.js
@@ -4,17 +4,23 @@
 const path = require("path");
 const { merge } = require("webpack-merge");
 const common = require("./webpack.config.common.js");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = merge(common, {
-  entry: {
-    "maplat_core": path.resolve(__dirname, "../tmpl/web-bridge.js")
-  },
-
   output: {
     path: path.resolve(__dirname, "../dist"),
     filename: '[name].js'
   },
 
-  plugins: [new HtmlWebpackPlugin({ template: "./public/index.html" })],
+  module: {
+    rules: [
+      {
+        test: /\.(jpg|jpeg|png)$/,
+        exclude: /node_modules/,
+        loader: 'file-loader',
+        options: {
+          outputPath: "images"
+        }
+      }
+    ]
+  },
 });
