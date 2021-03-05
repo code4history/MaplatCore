d1b6cf87078c49d25003ac56624b89a3fa566c60
Kohei Otsuka <kochizufan@gmail.com>
2021-03-05 15:51:03 +0900

    Update weiwudi.d.ts

--- src/types/weiwudi.d.ts
+++ src/types/weiwudi.d.ts
@@ -1,6 +1,11 @@
 declare module "weiwudi" {
   export default class Weiwudi {
-    public stats(): Promise<{ size?: number, count?: number, total?: number, percent?: number }>;
+    public stats(): Promise<{
+      size?: number;
+      count?: number;
+      total?: number;
+      percent?: number;
+    }>;
     public clean(): Promise<void>;
     public fetchAll(): Promise<void>;
     public cancel(): Promise<void>;
