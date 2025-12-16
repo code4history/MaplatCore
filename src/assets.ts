eb7a5f79b52e515d74fd9d8e19acbc18ab914384
kochizufan <kochizufan@gmail.com>
2025-12-17 01:27:36 +0900

    不要な @ts-ignore（および @ts-expect-error）を削除

--- src/assets.ts
+++ src/assets.ts
@@ -1,21 +1,12 @@
 // Import all assets used in the library
-// @ts-ignore
 import osm from "../parts/osm.jpg";
-// @ts-ignore
 import gsi from "../parts/gsi.jpg";
-// @ts-ignore
 import gsi_ortho from "../parts/gsi_ortho.jpg";
-// @ts-ignore
 import redcircle from "../parts/redcircle.png";
-// @ts-ignore
 import defaultpin_selected from "../parts/defaultpin_selected.png";
-// @ts-ignore
 import defaultpin from "../parts/defaultpin.png";
-// @ts-ignore
 import bluedot from "../parts/bluedot.png";
-// @ts-ignore
 import bluedot_transparent from "../parts/bluedot_transparent.png";
-// @ts-ignore
 import bluedot_small from "../parts/bluedot_small.png";
 
 // Export them as a single object or named exports
